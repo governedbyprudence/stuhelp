b34179accbaba58c907069e39e92147841c6f9b8
lazy <codingmadeeasy12@gmail.com>
2022-08-20 19:51:53 +0530

    some changes

--- backend/feedback/feedback/settings.py
+++ backend/feedback/feedback/settings.py
@@ -18,9 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%(2l%7g$ag5(u&)y26+ht!nb=6v25xz%)*wc1ffd*i5xdjk7^e'
+SECRET_KEY = open('../../.virtualenv/secretkey.txt','r').readline()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
